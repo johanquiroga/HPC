884761de27fb693bb4c736707e30228dfdb406ca
JohanQuiroga <quirogacj@utp.edu.co>
2017-03-26 21:18:50 -0500

    test matrix mult tiled

--- Cuda/Matrix_Mult/Tiling/test_cuda_matrix_mult_tiled.cu
+++ Cuda/Matrix_Mult/Tiling/test_cuda_matrix_mult_tiled.cu
@@ -123,52 +123,21 @@ void cudaKernelCall(float* h_A, float* h_B, float* h_C, int width) {
 
 	cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
 }
-	
-int main(int argc, char** argv) {
-	int width = TAM;
-	if(argc != 2) {
-		//printf("Matrix size missing!!, using %d\n", TAM);
-		//exit(EXIT_FAILURE);
-	} else {
-		width = atoi(argv[1]);
-		//printf("Matrix size: %d\n", width);
-	}
-	//printf("%d,", width);
-	clock_t start_serial, end_serial, start_cuda, end_cuda;
-	double time_used_serial, time_used_cuda;
-	//cudaError_t err = cudaSuccess;
-	//int size = width*width*sizeof(float);
-	float *h_A = (float *) malloc(size);
-	float *h_B = (float *) malloc(size);
-	float *h_C = (float *) malloc(size);
-	float *compare_C = (float *) malloc(size);
-	//float *d_A, *d_B, *d_C;
-	//float *d_A = NULL, *d_B = NULL, *d_C = NULL;
-	
-	//init matrix
-	for(int i=0; i<width*width; i++) {
-		h_A[i]=rand()%10;
-		h_B[i]=rand()%10;
-	}
-	
-	start_serial = clock();
-	matrixMultHost(h_A, h_B, compare_C, width);
-	end_serial = clock();
-	time_used_serial = ((double)(end_serial - start_serial))/CLOCKS_PER_SEC;
-	//printf("Tiempo en serial: %.10f\n", time_used_serial);
-	printf("%.10f,", time_used_serial);
 
-	/*err = cudaMalloc((void **)&d_A, size);
+void cudaTileKernelCall(float* h_A, float* h_B, float* h_C, int width) {
+	float *d_A, *d_B, *d_C;
+	cudaError_t err = cudaSuccess;
+	int size = width*width*sizeof(float);
+	err = cudaMalloc((void **)&d_A, size);
 	checkError(err, __LINE__);
 
 	err = cudaMalloc((void **)&d_B, size);
 	checkError(err, __LINE__);
 
 	err = cudaMalloc((void **)&d_C, size);
-	checkError(err, __LINE__);*/
-	
-	start_cuda = clock();
-	/*err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+	checkError(err, __LINE__);
+
+	err = cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
 	checkError(err, __LINE__);
 
 	err = cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
@@ -181,12 +150,61 @@ int main(int argc, char** argv) {
 	cudaDeviceSynchronize();
 	
 	err = cudaMemcpy(h_C, d_C, size, cudaMemcpyDeviceToHost);
-	checkError(err, __LINE__);*/
-	cudaKernelCall(h_A, h_B, h_C, width);
+	checkError(err, __LINE__);
+
+	cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
+}
+
+int main(int argc, char** argv) {
+	int width = TAM;
+	if(argc != 2) {
+		//printf("Matrix size missing!!, using %d\n", TAM);
+		//exit(EXIT_FAILURE);
+	} else {
+		width = atoi(argv[1]);
+		//printf("Matrix size: %d\n", width);
+	}
+	//printf("%d,", width);
+	clock_t start_serial, end_serial, start_cuda, end_cuda, start_tiled, end_tiled;
+	double time_used_serial, time_used_cuda, time_used_tiled;
+	int size = width*width*sizeof(float);
+	float *h_A = (float *) malloc(size);
+	float *h_B = (float *) malloc(size);
+	float *h_C = (float *) malloc(size);
+	float *h_C_tiled = (float *) malloc(size);
+	float *compare_C = (float *) malloc(size);
+	
+	//init matrix
+	for(int i=0; i<width*width; i++) {
+		h_A[i]=rand()%10;
+		h_B[i]=rand()%10;
+	}
+	/////////////////////Algoritmo serial//////////////////////////////////
+	start_serial = clock();
+	matrixMultHost(h_A, h_B, compare_C, width);
+	end_serial = clock();
+	time_used_serial = ((double)(end_serial - start_serial))/CLOCKS_PER_SEC;
+	//printf("Tiempo en serial: %.10f\n", time_used_serial);
+	printf("%.10f,", time_used_serial);	
+	///////////////////////////////////////////////////////////////////////
+	
+	/////////////////////Algoritmo paralelo///////////////////////////
+	start_cuda = clock();
+	cudaKernelCall(h_A, h_B, h_C_tiled, width);
 	end_cuda = clock();
 	time_used_cuda = ((double)(end_cuda - start_cuda))/CLOCKS_PER_SEC;
 	//printf("Tiempo en CUDA: %.10f\n", time_used_cuda);
 	printf("%.10f,", time_used_cuda);
+	//////////////////////////////////////////////////////////////////
+
+	/////////Algoritmo paralelo con memmoria compartida///////////////
+	start_tiled = clock();
+	cudaTileKernelCall(h_A, h_B, h_C, width);
+	end_tiled = clock();
+	time_used_tiled = ((double)(end_tiled - start_tiled))/CLOCKS_PER_SEC;
+	//printf("Tiempo en CUDA: %.10f\n", time_used_cuda);
+	printf("%.10f,", time_used_tiled);
+	//////////////////////////////////////////////////////////////////
 
 	//printMatrix(h_A, width);
 	//printMatrix(h_B, width);
@@ -194,15 +212,15 @@ int main(int argc, char** argv) {
 	//printMatrix(compare_C, width);
 	
 	//printf("\nAceleración obtenida: %.10f\n", time_used_serial/time_used_cuda);
-	printf("%.10f,", time_used_serial/time_used_cuda);
+	printf("%.10f,", time_used_serial/time_used_tiled);
 
-	if(compareResults(compare_C, h_C, width)) {
+	if(compareResults(compare_C, h_C_tiled, width)) {
 		printf("Good calculation\n");
 	} else {
 		printf("Bad calculation\n");
 	}
 	
-	free(h_A); free(h_B); free(h_C); free(compare_C);
+	free(h_A); free(h_B); free(h_C); free(compare_C); free(h_C_tiled);
 	//cudaFree(d_A); cudaFree(d_B); cudaFree(d_C);
 	return 0;
 }
