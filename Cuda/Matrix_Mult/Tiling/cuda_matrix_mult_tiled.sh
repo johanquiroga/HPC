b9b741317dcf947d1a35055d511f899ac0f3f07c
Johan Camilo Quiroga <quirogacj@utp.edu.co>
2017-03-26 20:31:49 -0500

    Matrix mult using tiles finished

---
+++ Cuda/Matrix_Mult/Tiling/cuda_matrix_mult_tiled.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+#SBATCH --job-name=cuda_matrix_mult_tiled
+#SBATCH --output=res_cuda_matrix_mult_tiled.csv
+#SBATCH --tasks=1
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:1
+
+#echo $CUDA_VISIBLE_DEVICES
+for size in {100,500,700,1000,2000}
+do
+	echo "Matrix size: $size x $size,,,,"
+	echo "iteracion,secuencial,cuda,aceleracion,resultado"
+	for run in {1..20}
+	do
+		#argv=$((run*100))
+		echo -n "$run,"
+		./build/cuda_matrix_mult_tiled $size
+	done
+done
