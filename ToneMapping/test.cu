1111f2076b67c6c57fa88ed8b6d40a76ec6a61f1
Johan Camilo Quiroga <quirogacj@utp.edu.co>
2017-05-18 21:03:08 -0500

    Succesfully reading .exr (hdr) images

--- ToneMapping/test.cu
+++ ToneMapping/test.cu
@@ -1,6 +1,11 @@
+#include <stdio.h>
 #include <cuda.h>
-#include <cv.h>
-#include <highgui.h>
+//#include <cv.h>
+//#include <highgui.h>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/photo.hpp>
+#include <vector>
 
 using namespace cv;
 
@@ -13,17 +18,32 @@ int main(int argc, char** argv)
 {
 	char* image_name = argv[1];
 	Mat hdr;
+	Size imageSize;
+	int width, height;
+//	std::vector<Mat>images;
 
+	printf("%s\n", image_name);
 	hdr = imread(image_name, CV_LOAD_IMAGE_ANYDEPTH);
 	if(argc !=2 || !hdr.data){
 	        printf("No image Data \n");
-        	return -1;
+      	return -1;
 	}
+
+//	images.push_back(hdr);
+//	Mat ldr;
+//	Ptr<TonemapDurand> tonemap = createTonemapDurand(2.2f);
+//	tonemap->process(images[0], ldr);
+//	imwrite("ldr.png", ldr * 255);
 
 	if(hdr.empty()) {
 		printf("Couldn't find or open the image...\n");
 		return -1;
 	}
+	imageSize = hdr.size();
+	width = imageSize.width;
+	height = imageSize.height;
+
+	printf("Width: %d\nHeight: %d\n", width, height);
 
 	return 0;
 }
