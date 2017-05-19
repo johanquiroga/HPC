2dfd6b2f166f8bfb66085ffc9bf06b4ac54015ba
Johan Camilo Quiroga <quirogacj@utp.edu.co>
2017-05-18 20:16:21 -0500

    Exposures update

--- ToneMapping/tone_mapping.cu
+++ ToneMapping/tone_mapping.cu
@@ -10,13 +10,23 @@
 
 using namespace cv;
 
+__device__ unsigned char clamp(int pixel) {
+	if(pixel < 0) {
+		pixel = 0;
+	} else if (pixel > 255) {
+		pixel = 255;
+	}
+	return (unsigned char)pixel;
+}
+
 __global__ void under_exposed(unsigned char* imageIn, unsigned char* imageOut, int width, int height, int channels) {
 	int Row = blockIdx.y*blockDim.y + threadIdx.y;
 	int Col = blockIdx.x*blockDim.x + threadIdx.x;
 	
 	if((Row < height) && (Col < width)) {
 		for(int i=0; i<channels; i++) {
-			imageOut[Row*width+Col*channels+i] = (int)(imageIn[Row*width+Col*channels+i] * 0.2);
+			imageOut[Row*width+Col*channels+i] = clamp(imageIn[Row*width+Col*channels+i] - 51);
+			//imageOut[Row*width+Col*channels+i] = 100;
 		}
 		// imageOut[(Row*width+Col)*3+BLUE] = (int)imageIn[(Row*width+Col)*3+BLUE] - 0.2;
 		// imageOut[(Row*width+Col)*3+GREEN] = (int)imageIn[(Row*width+Col)*3+GREEN] - 0.2;
@@ -24,7 +34,19 @@ __global__ void under_exposed(unsigned char* imageIn, unsigned char* imageOut, i
 	}
 }
 
-__global__ void over_exposed() {
+__global__ void over_exposed(unsigned char* imageIn, unsigned char* imageOut, int width, int height, int channels) {
+	int Row = blockIdx.y*blockDim.y + threadIdx.y;
+        int Col = blockIdx.x*blockDim.x + threadIdx.x;
+
+        if((Row < height) && (Col < width)) {
+                for(int i=0; i<channels; i++) {
+                        imageOut[Row*width+Col*channels+i] = clamp(imageIn[Row*width+Col*channels+i] + 51);
+                        //imageOut[Row*width+Col*channels+i] = 100;
+                }
+                // imageOut[(Row*width+Col)*3+BLUE] = (int)imageIn[(Row*width+Col)*3+BLUE] - 0.2;
+                // imageOut[(Row*width+Col)*3+GREEN] = (int)imageIn[(Row*width+Col)*3+GREEN] - 0.2;
+                // imageOut[(Row*width+Col)*3+RED] = (int)imageIn[(Row*width+Col)*3+RED] - 0.2;
+        }
 
 }
 
@@ -39,8 +61,8 @@ int main(int argc, char** argv) {
 	clock_t start_cuda, end_cuda;
 	double time_used_cuda;
 	int width, height, sizeImage;
-	Mat image, image_out_cuda;
-	unsigned char *h_ImageData, *d_ImageData, *d_ImageOut, *h_ImageOut;
+	Mat image, image_out_under, image_out_over;
+	unsigned char *h_ImageData, *d_ImageData, *d_ImageOut_Under, *d_ImageOut_Over, *h_ImageOut_Under, *h_ImageOut_Over;
 	Size imageSize; 
 
 	//printf("Image name: %s\n", image_name);
@@ -55,6 +77,8 @@ int main(int argc, char** argv) {
 		return -1;
 	}
 
+//	printf("Type: %d", image.depth());
+
 	imageSize = image.size();
 	width = imageSize.width;
 	height = imageSize.height;
@@ -63,18 +87,26 @@ int main(int argc, char** argv) {
 	
 	h_ImageData = (unsigned char *) malloc (sizeImage);
 	h_ImageData = image.data;
-	h_ImageOut = (unsigned char *) malloc (sizeImage);
+	h_ImageOut_Under = (unsigned char *) malloc (sizeImage);
+	h_ImageOut_Over = (unsigned char *) malloc (sizeImage);
 
 	err = cudaMalloc((void **)&d_ImageData, sizeImage);
 	if(err != cudaSuccess){
         	printf("Error reservando memoria para d_ImageData\n");
 	 	exit(-1);
 	}
-	err = cudaMalloc((void **)&d_ImageOut, sizeImage);
+	err = cudaMalloc((void **)&d_ImageOut_Under, sizeImage);
 	if(err != cudaSuccess){
-        	printf("Error reservando memoria para d_ImageOut\n");
+        	printf("Error reservando memoria para d_ImageOut_Under\n");
 	 	exit(-1);
 	}
+
+	err = cudaMalloc((void **)&d_ImageOut_Over, sizeImage);
+	if(err != cudaSuccess){
+        	printf("Error reservando memoria para d_ImageOut_Over\n");
+	 	exit(-1);
+	}
+
 
 	err = cudaMemcpy(d_ImageData, h_ImageData, sizeImage, cudaMemcpyHostToDevice);
 	if(err != cudaSuccess){
@@ -85,22 +117,37 @@ int main(int argc, char** argv) {
 	int blockSize = 32;
 	dim3 dimBlock(blockSize, blockSize, 1);
 	dim3 dimGrid(ceil(width/float(blockSize)), ceil(height/float(blockSize)), 1);
-	under_exposed<<<dimGrid, dimBlock>>>(d_ImageData, d_ImageOut, width, height, image.channels());
-		
-	err = cudaMemcpy(h_ImageOut, d_ImageOut, sizeImage, cudaMemcpyDeviceToHost);
+	under_exposed<<<dimGrid, dimBlock>>>(d_ImageData, d_ImageOut_Under, width, height, image.channels());
+	cudaDeviceSynchronize();
+	err = cudaMemcpy(h_ImageOut_Under, d_ImageOut_Under, sizeImage, cudaMemcpyDeviceToHost);
 	if(err != cudaSuccess){
-        	printf("Error copiando los datos de d_ImageOut a h_ImageOut\n");
+        	printf("Error copiando los datos de d_ImageOut_Under a h_ImageOut_Under\n");
 	 	exit(-1);
 	}
-	image_out_cuda.create(height, width, CV_8UC1);
-	image_out_cuda.data = h_ImageOut;
-	imwrite("image_out_cuda.jpg", image_out_cuda);
+
+	over_exposed<<<dimGrid, dimBlock>>>(d_ImageData, d_ImageOut_Over, width, height, image.channels());
+	cudaDeviceSynchronize();
+	err = cudaMemcpy(h_ImageOut_Over, d_ImageOut_Over, sizeImage, cudaMemcpyDeviceToHost);
+	if(err != cudaSuccess){
+        	printf("Error copiando los datos de d_ImageOut_Over a h_ImageOut_Under\n");
+	 	exit(-1);
+	}
+
+
+	image_out_under.create(height, width, CV_8UC1);
+	image_out_under.data = h_ImageOut_Under;
+	imwrite("image_out_under.jpg", image_out_under);
+
+	image_out_over.create(height, width, CV_8UC1);
+	image_out_over.data = h_ImageOut_Over;
+	imwrite("image_out_over.jpg", image_out_over);
+
 
 	//printf("Done\n\n");
-	showImage(image, "Image In");
-	showImage(image_out_cuda, "Image out CUDA");
+	//showImage(image, "Image In");
+	//showImage(image_out_cuda, "Image out CUDA");
 	//showImage(image_out_opencv, "Image out OpenCV");
 	//waitKey(0);
-	free(h_ImageOut); cudaFree(d_ImageData); cudaFree(d_ImageOut);
+	free(h_ImageOut_Under); free(h_ImageOut_Over); cudaFree(d_ImageData); cudaFree(d_ImageOut_Under); cudaFree(d_ImageOut_Over);
 	return 0;
 }
