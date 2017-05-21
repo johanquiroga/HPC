95c4700f1e08df1306bea01366f6467fc4888414
JohanQuiroga <quirogacj@utp.edu.co>
2017-05-20 20:33:48 -0500

    Testing gamma correction

--- ToneMapping/test.cu
+++ ToneMapping/test.cu
@@ -14,26 +14,26 @@
 using namespace cv;
 
 std::string type2str(int type) {
-    std::string r;
+	std::string r;
 
-    uchar depth = type & CV_MAT_DEPTH_MASK;
-    uchar chans = 1 + (type >> CV_CN_SHIFT);
+	uchar depth = type & CV_MAT_DEPTH_MASK;
+	uchar chans = 1 + (type >> CV_CN_SHIFT);
 
-    switch ( depth ) {
-        case CV_8U:  r = "8U"; break;
-        case CV_8S:  r = "8S"; break;
-        case CV_16U: r = "16U"; break;
-        case CV_16S: r = "16S"; break;
-        case CV_32S: r = "32S"; break;
-        case CV_32F: r = "32F"; break;
-        case CV_64F: r = "64F"; break;
-        default:     r = "User"; break;
-    }
+	switch ( depth ) {
+		case CV_8U:  r = "8U"; break;
+		case CV_8S:  r = "8S"; break;
+		case CV_16U: r = "16U"; break;
+		case CV_16S: r = "16S"; break;
+		case CV_32S: r = "32S"; break;
+		case CV_32F: r = "32F"; break;
+		case CV_64F: r = "64F"; break;
+		default:     r = "User"; break;
+	}
 
-    r += "C";
-    r += (chans+'0');
+	r += "C";
+	r += (chans+'0');
 
-    return r;
+	return r;
 }
 
 void checkError(cudaError_t err) {
@@ -120,49 +120,49 @@ void checkError(cudaError_t err) {
 
 __device__ float gamma_correction(float f_stop, float gamma, float val)
 {
-    return powf((val*powf(2,f_stop)),(1.0/gamma));
+	return powf((val*powf(2,f_stop)),(1.0/gamma));
 }
 
 __global__ void tonemap(float* imageIn, float* imageOut, int width, int height, int channels, int depth, float f_stop,
-                        float gamma)
+						float gamma)
 {
-    int Row = blockDim.y * blockIdx.y + threadIdx.y;
-    int Col = blockDim.x * blockIdx.x + threadIdx.x;
+	int Row = blockDim.y * blockIdx.y + threadIdx.y;
+	int Col = blockDim.x * blockIdx.x + threadIdx.x;
 
-    if(Row < height && Col < width) {
-        imageOut[(Row*width+Col)*3+BLUE] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+BLUE]);
-        imageOut[(Row*width+Col)*3+GREEN] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+GREEN]);
-        imageOut[(Row*width+Col)*3+RED] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+RED]);
-    }
+	if(Row < height && Col < width) {
+		imageOut[(Row*width+Col)*3+BLUE] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+BLUE]);
+		imageOut[(Row*width+Col)*3+GREEN] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+GREEN]);
+		imageOut[(Row*width+Col)*3+RED] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+RED]);
+	}
 }
 
  void showImage(Mat &image, const char *window) {
- 	namedWindow(window, CV_WINDOW_NORMAL);
- 	imshow(window, image);
+	namedWindow(window, CV_WINDOW_NORMAL);
+	imshow(window, image);
  }
 
 int main(int argc, char** argv)
 {
 	char* image_name = argv[1];
-    float *h_ImageData, *d_ImageData, *d_ImageOut, *h_ImageOut;
+	float *h_ImageData, *d_ImageData, *d_ImageOut, *h_ImageOut;
 	Mat hdr, ldr;
 	Size imageSize;
 	int width, height, channels, sizeImage;
-    float f_stop=0.0, gamma=0.0;
-    bool show_flag = false;
+	float f_stop=0.0, gamma=0.0;
+	bool show_flag = false;
 //	std::vector<Mat>images;
 
 	printf("%s\n", image_name);
 	hdr = imread(image_name, -1);
 	if(argc !=5 || !hdr.data) {
-        printf("No image Data \n");
-        printf("Usage: ./test <file_path> <f_stop> <gamma> <show_flag>");
-      	return -1;
+		printf("No image Data \n");
+		printf("Usage: ./test <file_path> <f_stop> <gamma> <show_flag>");
+		return -1;
 	}
 
-    f_stop = atof(argv[2]);
-    gamma = atof(argv[3]);
-    show_flag = argv[4];
+	f_stop = atof(argv[2]);
+	gamma = atof(argv[3]);
+	show_flag = argv[4];
 
 	if(hdr.empty()) {
 		printf("Couldn't find or open the image...\n");
@@ -171,8 +171,8 @@ int main(int argc, char** argv)
 	imageSize = hdr.size();
 	width = imageSize.width;
 	height = imageSize.height;
-    channels = hdr.channels();
-    sizeImage = sizeof(float)*width*height*channels;
+	channels = hdr.channels();
+	sizeImage = sizeof(float)*width*height*channels;
 
 	//printf("Width: %d\nHeight: %d\n", width, height);
 	//std::string ty =  type2str( hdr.type() );
@@ -180,32 +180,33 @@ int main(int argc, char** argv)
 
 	//printf("Channels: %d\nDepth: %d\n", hdr.channels(), hdr.depth());
 
-    h_ImageData = (float *) malloc (sizeImage);
-    h_ImageData = hdr.data;
-    h_ImageOut = (float *) malloc (sizeImage);
+	h_ImageData = (float *) malloc (sizeImage);
+	h_ImageData = (float *)hdr.data;
+	h_ImageOut = (float *) malloc (sizeImage);
 
-    checkError(cudaMalloc((void **)&d_ImageData, sizeImage));
-    checkError(cudaMalloc((void **)&d_ImageOut, sizeImage));
-    checkError(cudaMemcpy(d_ImageData, h_ImageData, sizeImage, cudaMemcpyHostToDevice));
+	checkError(cudaMalloc((void **)&d_ImageData, sizeImage));
+	checkError(cudaMalloc((void **)&d_ImageOut, sizeImage));
+	checkError(cudaMemcpy(d_ImageData, h_ImageData, sizeImage, cudaMemcpyHostToDevice));
 
-    int blockSize = 32;
-    dim3 dimBlock(blockSize, blockSize, 1);
-    dim3 dimGrid(ceil(width/float(blockSize)), ceil(height/float(blockSize)), 1);
-    tonemap<<<dimGrid, dimBlock>>>(d_ImageData, d_ImageOut, width, height, channels, 32, f_stop, gamma);
-    cudaDeviceSynchronize();
+	int blockSize = 32;
+	dim3 dimBlock(blockSize, blockSize, 1);
+	dim3 dimGrid(ceil(width/float(blockSize)), ceil(height/float(blockSize)), 1);
+	tonemap<<<dimGrid, dimBlock>>>(d_ImageData, d_ImageOut, width, height, channels, 32, f_stop, gamma);
+	cudaDeviceSynchronize();
 
-    checkError(cudaMemcpy(h_ImageOut, d_ImageOut, sizeImage, cudaMemcpyDeviceToHost));
+	checkError(cudaMemcpy(h_ImageOut, d_ImageOut, sizeImage, cudaMemcpyDeviceToHost));
 
-    ldr.create(height, width, CV_8UC3);
-    ldr.data = h_ImageOut;
-    imwrite("ldr.png", ldr*255);
+	ldr.create(height, width, CV_32FC3);
+	ldr.data = (unsigned char *)h_ImageOut;
+	ldr.converTo(ldr, CV_8UC3, 255);
+	imwrite("ldr.png", ldr*255);
 
-    if(show_flag) {
-        showImage(ldr, "Image out LDR");
-        waitKey(0);
-    }
+	if(show_flag) {
+		showImage(ldr, "Image out LDR");
+		waitKey(0);
+	}
 
-    free(h_ImageOut); cudaFree(d_ImageData); cudaFree(d_ImageOut);
+	free(h_ImageOut); cudaFree(d_ImageData); cudaFree(d_ImageOut);
 
 	return 0;
 }
