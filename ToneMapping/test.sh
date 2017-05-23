d6e9a89d441e23c6e3b945f218f99adab138aaed
JohanQuiroga <quirogacj@utp.edu.co>
2017-05-22 22:37:49 -0500

    Testing MPI

--- ToneMapping/test.sh
+++ ToneMapping/test.sh
@@ -2,22 +2,24 @@
 #
 #SBATCH --job-name=test_tone_mapping
 #SBATCH --output=res_test_tone_mapping.txt
-#SBATCH --tasks=2
-#SBATCH --nodes=2
+#SBATCH --tasks=1
+#SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 
-for i in {1..5}
-do
+mpirun ./build/tonemapping 0.4 1.2 ./images ./results
+
+#for i in {1..5}
+#do
 	#echo "**Image**: img$i.jpg"
 	#echo
 	#echo "iteracion|Host|OpenCV|aceleracion Host-OCV|OpenCVGPU|aceleracion OCV-OCVGPU|Cuda|aceleracion OCV-Cuda|aceleracion OCVGPU-Cuda"
 	#echo ":---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:|:---:"
-	for run in {1..20}
-	do
+#	for run in {1..20}
+#	do
 		#argv=$((run*100))
 		#echo -n "$run|"
-		mpirun ./build/tonemapping 0.4 1.2 ./images ./results
-	done
+#		mpirun ./build/tonemapping 0.4 1.2 ./images ./results
+#	done
 	#echo "Promedios:| | | | | | | | "
 	#echo
-done
+#done
