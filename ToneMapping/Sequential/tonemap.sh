b3a8c91c11d0c294ff9e6cbd5c31d26ced8f1d05
Johan Camilo Quiroga <quirogacj@utp.edu.co>
2017-05-28 13:21:29 -0500

    Updated slurm script

--- ToneMapping/Sequential/tonemap.sh
+++ ToneMapping/Sequential/tonemap.sh
@@ -2,8 +2,11 @@
 #
 #SBATCH --job-name=tonemap
 #SBATCH --output=res_tonemap.md
+#SBATCH --tasks=1
+#SBATCH --nodes=1
+
 
 echo "imagen|tiempo"
 echo ":---:|:---:"
 
-srun tonemap 0.4 1.2 images results \|
+srun tonemap 0.4 1.2 ../images results \|
