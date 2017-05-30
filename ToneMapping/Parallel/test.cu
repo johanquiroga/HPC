d6ceff0c4bddfa8de0096bada764e36f12ddb75d
JohanQuiroga <quirogacj@utp.edu.co>
2017-05-30 18:16:55 -0500

    Testing new operators

--- ToneMapping/Parallel/test.cu
+++ ToneMapping/Parallel/test.cu
@@ -1,229 +1,149 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <cuda.h>
+#include <vector>
+#include <string>
+#include <time.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <vector>
-#include <string>
 
+#include "lib/helpers.h"
+#include "lib/tonemap.cuh"
+
+#define BLOCK_SIZE 32
 #define BLUE 0
 #define GREEN 1
 #define RED 2
 
 using namespace cv;
 
-std::string type2str(int type) {
-	std::string r;
+//void checkError(cudaError_t err) {
+//	if(err!=cudaSuccess) {
+//		printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
+//		exit(EXIT_FAILURE);
+//	}
+//}
 
-	uchar depth = type & CV_MAT_DEPTH_MASK;
-	uchar chans = 1 + (type >> CV_CN_SHIFT);
-
-	switch ( depth ) {
-		case CV_8U:  r = "8U"; break;
-		case CV_8S:  r = "8S"; break;
-		case CV_16U: r = "16U"; break;
-		case CV_16S: r = "16S"; break;
-		case CV_32S: r = "32S"; break;
-		case CV_32F: r = "32F"; break;
-		case CV_64F: r = "64F"; break;
-		default:     r = "User"; break;
-	}
-
-	r += "C";
-	r += (chans+'0');
-
-	return r;
-}
-
-void checkError(cudaError_t err) {
-	if(err!=cudaSuccess) {
-		printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
-		exit(EXIT_FAILURE);
-	}
-}
-
-///***********************************
-//I = 1/61*(Red*20 + Green*40 + Blue)
-//***********************************/
-//__device__ float compute_intensity(float Blue, float Green, float Red)
-//{
-//	return ((1.0/61.0) * (Blue + (Green * 40) + (Red * 20)));
-//}
-//
-///**********************
-//Red/I, Green/I, Blue/I
-//**********************/
-//__device__ float compute_chrominance(float Channel, float I)
-//{
-//	return Channel/I;
-//}
-//
-///***********
-//L = log2(I)
-//***********/
-//__device__ float compute_intensity_log(float I)
-//{
-//	return log2f(I);
-//}
-//
-///*********
-//B = bf(L)
-//*********/
-//__device__ float apply_billateral_filter(float L)
-//{
-//	//@TODO
-//}
-//
-///*******
-//D = L-B
-//*******/
-//__device__ float compute_detail_layer(float L, float B)
-//{
-//	return L-B;
-//}
-//
-///********************
-//nB = (B-offset)*scale
-//********************/
-//__device__ float apply_offset_scale_base(float B, int offset, int scale)
-//{
-//	return (B-offset)*scale;
-//}
-//
-///*************
-//O = exp(nB+D)
-//*************/
-//__device__ float reconstruct_log_intensity(float nB, float D)
-//{
-//	return expf(nB+D);
-//}
-//
-///****************************
-//nR,nG,nB = O*(R/I, G/I, B/I)
-//****************************/
-//__device__ float put_colors_back(float Channel, float I, float O)
-//{
-//	return O*(compute_chrominance(Channel, I));
-//}
-//
-///*********************
-//
-//*********************/
-//__global__ void tonemap(float* imageIn, float* imageOut, int width, int height, int channels, int depth)
-//{
-//	//Each thread reads each pixel and puts it through the pipeline
-//}
-//
-
-// Iout(x,y)=(Iin(x,y)⋅2ᶺf)ᶺ(1/g)
-
-__global__ void find_maximum_kernel(float *array, float *max, int *mutex, unsigned int n)
+float task(std::string image_name, std::string images_path, std::string dst_path, std::string tmo, int blockSize,
+           float f_stop, float gamma, float q, float k, float b)
 {
-	unsigned int index = threadIdx.x + blockIdx.x*blockDim.x;
-	unsigned int stride = gridDim.x*blockDim.x;
-	unsigned int offset = 0;
-//	const int size = blockSize;
+	float *h_ImageData, *h_ImageOut;
+	std::string image_out_name;
+	Mat hdr, ldr;
+	int width, height, channels, sizeImage;
 
-	extern	__shared__ float cache[];
+	std::string path = images_path + "/" + image_name;
 
+	hdr = imread(path.c_str(), -1);
 
-	float temp = -1.0;
-	while(index + offset < n){
-		temp = fmaxf(temp, array[index + offset]);
-
-		offset += stride;
+	if(!hdr.data) {
+		printf("No image Data \n");
+		return EXIT_FAILURE;
+//        return -1;
 	}
 
-	cache[threadIdx.x] = temp;
-
-	__syncthreads();
-
-
-	// reduction
-	unsigned int i = blockDim.x/2;
-	while(i != 0){
-		if(threadIdx.x < i){
-			cache[threadIdx.x] = fmaxf(cache[threadIdx.x], cache[threadIdx.x + i]);
-		}
-
-		__syncthreads();
-		i /= 2;
+	if(hdr.empty()) {
+		printf("Couldn't find or open the image...\n");
+		return EXIT_FAILURE;
+//        return -1;
 	}
 
-	if(threadIdx.x == 0){
-		while(atomicCAS(mutex,0,1) != 0);  //lock
-		*max = fmaxf(*max, cache[0]);
-		atomicExch(mutex, 0);  //unlock
+	width = hdr.cols;
+	height = hdr.rows;
+	channels = hdr.channels();
+	sizeImage = sizeof(float)*width*height*channels;
+
+	h_ImageData = (float *)hdr.data;
+	h_ImageOut = (float *) malloc (sizeImage);
+	float elapsed_time = 0.0;
+
+	if(tmo == "log") {
+		elapsed_time = log_tonemap(h_ImageData, h_ImageOut, width, height, channels, k, q, blockSize, sizeImage);
+	} else if(tmo == "gamma") {
+		elapsed_time = gamma_tonemap(h_ImageData, h_ImageOut, width, height, channels, f_stop, gamma, blockSize,
+		                             sizeImage);
+	} else {
+		elapsed_time = adaptive_log_tonemap(h_ImageData, h_ImageOut, width, height, channels, b, blockSize, sizeImage);
 	}
+
+	ldr.create(height, width, CV_32FC3);
+	ldr.data = (unsigned char *)h_ImageOut;
+	ldr.convertTo(ldr, CV_8UC3, 255);
+	image_out_name = dst_path + "/" + change_image_extension(image_name);
+	imwrite(image_out_name.c_str(), ldr);
+
+	free(h_ImageOut);
+
+	return elapsed_time;
 }
 
-__device__ float gamma_correction(float f_stop, float gamma, float val)
+void Usage()
 {
-	return powf((val*powf(2,f_stop)),(1.0/gamma));
-}
-
-__device__ float logarithmic_mapping(float lw_max, float ld_max, float lw, float b)
-{
-	float ld = (ld_max)/(100*log10f(1+lw_max)*((logf(1+lw))/(logf(2+8*powf((lw/lw_max),(logf(b)/logf(0.5)))))));
-	return ld;
-}
-
-__global__ void tonemap(float* imageIn, float* imageOut, int width, int height, int channels, int depth, float f_stop,
-						float gamma, float* max, float b)
-{
-	int Row = blockDim.y * blockIdx.y + threadIdx.y;
-	int Col = blockDim.x * blockIdx.x + threadIdx.x;
-
-	if(Row < height && Col < width) {
-		imageOut[Row*width+Col] =  logarithmic_mapping(*max, 100.0, imageIn[Row*width+Col], b);
-//		imageOut[(Row*width+Col)*3+BLUE] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+BLUE]);
-//		imageOut[(Row*width+Col)*3+GREEN] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+GREEN]);
-//		imageOut[(Row*width+Col)*3+RED] = gamma_correction(f_stop, gamma, imageIn[(Row*width+Col)*3+RED]);
-	}
-}
-
-void showImage(Mat &image, const char *window) {
-	namedWindow(window, CV_WINDOW_NORMAL);
-	imshow(window, image);
+	printf("Usage: ./test <images_src> <results_dst> <output_separator> <TMO>(log/gamma/adap_log)\n");
+	printf("If TMO = log, add: <k> <q>\n");
+	printf("If TMO = gamma, add: <gamma> <f_stop>\n");
+	printf("If TMO = adap_log, add: <b>\n");
 }
 
 int main(int argc, char** argv)
 {
-	char* image_name = argv[1];
-    char* image_out_name = argv[3];
-	float *h_ImageData, *d_ImageData, *d_ImageOut, *h_ImageOut, *h_max, *d_max;
-	int *d_mutex, N;
-	Mat hdr, ldr, gray_hdr;
-	Size imageSize;
-	int width, height, channels, sizeImage;
-	float f_stop=0.0, gamma=0.0, b;
-	int show_flag = 0;
-//	std::vector<Mat>images;
+	float f_stop=0.0, gamma=0.0, q=0.0, k=0.0, b=1.0;
 
-//	printf("%s\n", image_name);
-	hdr = imread(image_name, -1);
-	if(argc !=4 || !hdr.data) {
-		printf("No image Data \n");
-		//printf("Usage: ./test <file_path> <f_stop> <gamma> <show_flag> <output_file_path>");
-		printf("Usage: ./test <file_path> <b> <output_file_path>");
-		return -1;
+	if(argc == 1 || argc < 5) {
+		Usage();
+		return EXIT_FAILURE;
+	}
+	std::string images_path(argv[1]);
+	std::string dst_path(argv[2]);
+	std::string separator(argv[3]);
+	std::string tmo(argv[4]);
+
+	if(tmo == "log") {
+		if(argc != 7) {
+			Usage();
+			return EXIT_FAILURE;
+		}
+		k = atof(argv[5]);
+		q = atof(argv[6]);
+	} else if(tmo == "gamma") {
+		if(argc != 7) {
+			Usage();
+			return EXIT_FAILURE;
+		}
+		gamma = atof(argv[5]);
+		f_stop = atof(argv[6]);
+	} else if(tmo == "adap_log") {
+		if(argc != 6) {
+			Usage();
+			return EXIT_FAILURE;
+		}
+		b = atof(argv[5]);
+	} else {
+		Usage();
+		return EXIT_FAILURE;
 	}
 
-	b = atof(argv[2]);
-//	f_stop = atof(argv[2]);
-//	gamma = atof(argv[3]);
-//	show_flag = atoi(argv[4]);
+	clock_t start, end;
+	double batch_time;
+	std::vector<std::string> files;
+	read_files(files, images_path);
+	int blockSize = BLOCK_SIZE;
 
-	if(hdr.empty()) {
-		printf("Couldn't find or open the image...\n");
-		return -1;
+	start = clock();
+	while(!files.empty()) {
+		float elapsed_time = 0.0;
+		std::string file_name = files.back();
+		elapsed_time = task(file_name, images_path, dst_path, tmo, blockSize, f_stop, gamma, q, k, b);
+		printTime(file_name, elapsed_time, separator);
+		files.pop_back();
 	}
+	end = clock();
+	batch_time = ((double)(end - start))/CLOCKS_PER_SEC;
+	printTime("batch time", batch_time, separator);
 
-	cvtColor(hdr, gray_hdr, CV_BGR2GRAY);
-//	normalize(gray_hdr, gray_hdr, 0.0, 1.0, NORM_MINMAX, CV_32FC1);
+	return EXIT_SUCCESS;
 
-	imageSize = gray_hdr.size();
+/*	imageSize = gray_hdr.size();
 	width = imageSize.width;
 	height = imageSize.height;
 	channels = gray_hdr.channels();
@@ -289,5 +209,5 @@ int main(int argc, char** argv)
 
 	free(h_max); free(h_ImageOut); cudaFree(d_ImageData); cudaFree(d_ImageOut); cudaFree(d_max); cudaFree(d_mutex);
 
-	return 0;
+	return 0;*/
 }
