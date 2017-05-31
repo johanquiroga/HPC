0b727d283bd89679c56a0c25869c310a404ab17f
JohanQuiroga <quirogacj@utp.edu.co>
2017-05-30 19:33:33 -0500

    Updated files

--- ToneMapping/Parallel/tonemap.cu
+++ ToneMapping/Parallel/tonemap.cu
@@ -17,19 +17,6 @@
 
 using namespace cv;
 
-//void checkError(cudaError_t err) {
-//	if(err!=cudaSuccess) {
-//		printf("%s in %s at line %d\n", cudaGetErrorString(err), __FILE__, __LINE__);
-//		exit(EXIT_FAILURE);
-//	}
-//}
-
-void my_abort(int err)
-{
-	std::cout << "Program FAILED\n";
-    exit(err);
-}
-
 float task(std::string image_name, std::string images_path, std::string dst_path, std::string tmo, int blockSize,
            float f_stop, float gamma, float q, float k, float b)
 {
@@ -85,7 +72,7 @@ float task(std::string image_name, std::string images_path, std::string dst_path
 
 void Usage()
 {
-	printf("Usage: ./test <images_src> <results_dst> <output_separator> <TMO>(log/gamma)\n");
+	printf("Usage: ./tonemap <images_src> <results_dst> <output_separator> <TMO>(log/gamma)\n");
 	printf("If TMO = log, add: <k> <q>\n");
 	printf("If TMO = gamma, add: <gamma> <f_stop>\n");
 //	printf("If TMO = adap_log, add: <b>\n");
@@ -148,72 +135,4 @@ int main(int argc, char** argv)
 	printTime("batch time", batch_time, separator);
 
 	return EXIT_SUCCESS;
-
-/*	imageSize = gray_hdr.size();
-	width = imageSize.width;
-	height = imageSize.height;
-	channels = gray_hdr.channels();
-	N = width*height*channels;
-	sizeImage = sizeof(float)*width*height*channels;
-
-	//printf("Width: %d\nHeight: %d\n", width, height);
-	std::string ty =  type2str( gray_hdr.type() );
-	printf("Image: %s %dx%d \n", ty.c_str(), hdr.cols, hdr.rows );
-
-	//printf("Channels: %d\nDepth: %d\n", hdr.channels(), hdr.depth());
-
-	//h_ImageData = (float *) malloc (sizeImage);
-	h_ImageData = (float *)gray_hdr.data;
-	h_max = (float*)malloc(sizeof(float));
-	h_ImageOut = (float *) malloc (sizeImage);
-
-	cudaEvent_t start, stop;
-	cudaEventCreate(&start);
-	cudaEventCreate(&stop);
-	float milliseconds = 0;
-
-	checkError(cudaMalloc((void **)&d_ImageData, sizeImage));
-	checkError(cudaMalloc((void **)&d_ImageOut, sizeImage));
-	checkError(cudaMalloc((void**)&d_max, sizeof(float)));
-	checkError(cudaMalloc((void**)&d_mutex, sizeof(int)));
-	checkError(cudaMemset(d_max, 0, sizeof(float)));
-	checkError(cudaMemset(d_mutex, 0, sizeof(int)));
-	checkError(cudaMemcpy(d_ImageData, h_ImageData, sizeImage, cudaMemcpyHostToDevice));
-
-	int blockSize = 256;
-	dim3 dimBlock(blockSize, 1, 1);
-	dim3 dimGrid(ceil(width/float(blockSize)), 1, 1);
-	find_maximum_kernel<<< dimGrid, dimBlock, sizeof(float)*blockSize >>>(d_ImageData, d_max, d_mutex, N);
-	cudaDeviceSynchronize();
-	//cudaMemcpy(h_max, d_max, sizeof(float), cudaMemcpyDeviceToHost);
-
-	blockSize = 32;
-	dim3 dimBlock2(blockSize, blockSize, 1);
-	dim3 dimGrid2(ceil(width/float(blockSize)), ceil(height/float(blockSize)), 1);
-	cudaEventRecord(start);
-	tonemap<<<dimGrid2, dimBlock2>>>(d_ImageData, d_ImageOut, width, height, channels, 32, f_stop, gamma, d_max, b);
-	cudaEventRecord(stop);
-	cudaDeviceSynchronize();
-	cudaEventElapsedTime(&milliseconds, start, stop);
-	printf("%s|%.10f\n", image_name, milliseconds/1000.0);
-
-	checkError(cudaMemcpy(h_ImageOut, d_ImageOut, sizeImage, cudaMemcpyDeviceToHost));
-
-	ldr.create(height, width, CV_32FC1);
-	ldr.data = (unsigned char *)h_ImageOut;
-	ldr.convertTo(ldr, CV_8UC1, 1);
-	//cvtColor(ldr, ldr, CV_GRAY2BGR);
-	imwrite(image_out_name, ldr);
-
-    ty =  type2str( ldr.type() );
-    printf("Image result: %s %dx%d \n", ty.c_str(), ldr.cols, ldr.rows );
-
-	if(show_flag) {
-		showImage(ldr, "Image out LDR");
-		waitKey(0);
-	}
-
-	free(h_max); free(h_ImageOut); cudaFree(d_ImageData); cudaFree(d_ImageOut); cudaFree(d_max); cudaFree(d_mutex);
-
-	return 0;*/
 }
--- ToneMapping/Parallel/tonemap.sh
+++ ToneMapping/Parallel/tonemap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 #SBATCH --job-name=test_tone_mapping
-#SBATCH --output=res_tone_mapping.md
+#SBATCH --output=res_tone_mapping_gamma.md
 #SBATCH --tasks=1
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
