8ce7a096849fd2b30b003c0adbe4ad88dae11f3a
Johan Camilo Quiroga <quirogacj@utp.edu.co>
2017-05-30 20:08:35 -0500

    Slurm script updated

--- ToneMapping/Parallel/tonemap.sh
+++ ToneMapping/Parallel/tonemap.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #
-#SBATCH --job-name=test_tone_mapping
-#SBATCH --output=res_tone_mapping_gamma.md
+#SBATCH --job-name=tone_mapping
 #SBATCH --tasks=1
 #SBATCH --nodes=1
 #SBATCH --gres=gpu:1
 
-TMO="gamma"
+TMO="log"
 
 echo "imagen|tiempo"
 echo ":---:|:---:"
 
-srun tonemap ../images results_$TMO \| $TMO 1.2 0.4
+srun tonemap ../images results/results_$TMO \| $TMO 1 1
