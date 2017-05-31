0975cc566dadf1c8696004257d253bb2b4a98411
JohanQuiroga <quirogacj@utp.edu.co>
2017-05-30 22:46:03 -0500

    Slurm script updated

--- ToneMapping/Sequential/tonemap.sh
+++ ToneMapping/Sequential/tonemap.sh
@@ -13,20 +13,20 @@
 #srun tonemap ../images results/results_$TMO \| $TMO 1 1 #log
 #srun tonemap ../images results/results_$TMO \| $TMO 1 150 #adap_log
 
-for tmo in {"gamma", "log", "adap_log"}
+for tmo in "gamma" "log" "adap_log"
 do
 	echo "**Operador: $tmo**"
 	echo
 	echo "imagen|tiempo"
 	echo ":---:|:---:"
-	if $tmo == "gamma"; then
-		echo "$tmo"
+	if ["${tmo}" == "gamma"]; then
+		echo "${tmo}"
 	    #srun tonemap ../images results/results_$tmo \| $tmo 1.2 0.4 #gamma
-	elif $tmo == "log"; then
-	    echo "$tmo"
+	elif ["${tmo}" == "log"]; then
+	    echo "${tmo}"
 	    #srun tonemap ../images results/results_$tmo \| $tmo 1 1 #log
-	elif $tmo == "adap_log"; then
-		echo "$tmo"
+	elif ["${tmo}" == "adap_log"]; then
+		echo "${tmo}"
 		#srun tonemap ../images results/results_$tmo \| $tmo 1 150 #adap_log
 	fi
 	echo
