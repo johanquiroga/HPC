8f07c1e15b3169fbaabb58983d9b542d3a978356
JohanQuiroga <quirogacj@utp.edu.co>
2017-05-30 22:54:40 -0500

    Slurm script updated

--- ToneMapping/MPI/tonemapping.sh
+++ ToneMapping/MPI/tonemapping.sh
@@ -4,11 +4,30 @@
 #SBATCH --ntasks=3
 #SBATCH --gres=gpu:1
 
-TMO="gamma"
-
-echo "imagen|tiempo|id worker"
-echo ":---:|:---:|:---:"
-
-mpirun build/tonemapping ../images results/results_$TMO \| $TMO 1.2 0.4 #gamma
+#TMO="gamma"
+#
+#echo "imagen|tiempo|id worker"
+#echo ":---:|:---:|:---:"
+#
+#mpirun build/tonemapping ../images results/results_$TMO \| $TMO 1.2 0.4 #gamma
 #mpirun build/tonemapping ../images results/results_$TMO \| $TMO 1 1 #log
 #mpirun build/tonemapping ../images results/results_$TMO \| $TMO 1 150 #adap_log
+
+for tmo in "gamma" "log" "adap_log"
+do
+	echo "**Operador: $tmo**"
+	echo
+	echo "imagen|tiempo"
+	echo ":---:|:---:"
+	if [ "${tmo}" == "gamma" ]; then
+#		echo "${tmo}"
+		mpirun build/tonemapping ../images results/results_$tmo \| $tmo 1.2 0.4 #gamma
+	elif [ "${tmo}" == "log" ]; then
+#	    echo "${tmo}"
+		mpirun build/tonemapping ../images results/results_$tmo \| $tmo 1 1 #log
+	elif [ "${tmo}" == "adap_log" ]; then
+#		echo "${tmo}"
+		mpirun build/tonemapping ../images results/results_$tmo \| $tmo 1 150 #adap_log
+	fi
+	echo
+done
