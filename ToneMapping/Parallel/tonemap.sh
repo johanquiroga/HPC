cc2aa0e364cb3b26fecfd1566d81836e318f6a15
JohanQuiroga <quirogacj@utp.edu.co>
2017-05-30 20:28:21 -0500

    Updated slurm script

--- ToneMapping/Parallel/tonemap.sh
+++ ToneMapping/Parallel/tonemap.sh
@@ -10,4 +10,6 @@ TMO="log"
 echo "imagen|tiempo"
 echo ":---:|:---:"
 
-srun tonemap ../images results/results_$TMO \| $TMO 1 1
+#srun tonemap ../images results/results_$TMO \| $TMO 1.2 0.4 #gamma
+srun tonemap ../images results/results_$TMO \| $TMO 1 1 #log
+#srun tonemap ../images results/results_$TMO \| $TMO 1 #adap_log
